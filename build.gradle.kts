201caf1e5c3808ce0696952069893e5200f9a806
Igor <igor.sakhankov@glovoapp.com>
2024-02-06 06:58:18 +0100

    chore: clean ups and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,3 @@
-plugins {
-    id("java")
-    id("io.freefair.lombok") version "8.4"
-}
-
 allprojects {
     group = "com.harbour"
     version = "0.0.1"
@@ -10,8 +5,4 @@ allprojects {
 
 repositories {
     mavenCentral()
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
--- springboot/build.gradle.kts
+++ springboot/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
     id("com.netflix.dgs.codegen") version "6.0.3"
+    id("io.github.lognet.grpc-spring-boot") version "5.1.5"
 }
 
 repositories {
